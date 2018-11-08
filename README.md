f1784e09b0b966090a65529d7b902e95f8572e0e
kai <kai@cloudpack.jp>
2018-11-08 16:54:12 +0900

    PythonでAmazon Cognitoのユーザープールを作成してみた

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # create-cognito-user-pool-at-python
-PythonでAmazon Cognitoのユーザープールを作成してみる
+
+PythonでAmazon Cognitoのユーザープールを作成してみる  
+
+
+## Usage
+
+```sh
+> git clone https://github.com/kai-kou/create-cognito-user-pool-at-python.git
+> cd create-cognito-user-pool-at-python
+> python -m venv venv
+> . venv/bin/activate
+> pip install -r requirements.txt
+
+# ユーザープール名、ユーザー名、パスワードを編集
+> vi main.py
+
+> python main.py
+
+> open https://ap-northeast-1.console.aws.amazon.com/cognito/users
+```
---
+++ main.py
@@ -0,0 +1,204 @@
+import boto3
+
+
+def main():
+  client = boto3.client('cognito-idp')
+
+  poot_name = '[Cognitoのユーザープール名]'
+
+  # ユーザープールの作成
+  user_pool = _create_user_pool(client, poot_name)
+
+  # アプリクライアントの作成
+  _create_user_pool_app_client(client, user_pool)
+
+  # TOTPを利用したMFAを有効にする
+  _set_user_pool_mfa_config(client, user_pool)
+
+  _create_user(client, user_pool, '[username]', '[password]')
+
+
+def _create_user_pool(client, pool_name):
+  response = client.create_user_pool(
+    PoolName=pool_name,
+    Policies={
+      'PasswordPolicy': {
+        'MinimumLength': 8,
+        'RequireUppercase': True,
+        'RequireLowercase': True,
+        'RequireNumbers': True,
+        'RequireSymbols': True
+      }
+    },
+    # LambdaConfig={
+    #   'PreSignUp': 'string',
+    #   'CustomMessage': 'string',
+    #   'PostConfirmation': 'string',
+    #   'PreAuthentication': 'string',
+    #   'PostAuthentication': 'string',
+    #   'DefineAuthChallenge': 'string',
+    #   'CreateAuthChallenge': 'string',
+    #   'VerifyAuthChallengeResponse': 'string',
+    #   'PreTokenGeneration': 'string',
+    #   'UserMigration': 'string'
+    # },
+    # AutoVerifiedAttributes=[
+    #   'phone_number'|'email',
+    # ],
+    # AliasAttributes=[
+    #   'phone_number'|'email'|'preferred_username',
+    # ],
+    # UsernameAttributes=[
+    #   'phone_number'|'email',
+    # ],
+    SmsVerificationMessage=' 検証コードは {####} です。',
+    EmailVerificationMessage=' 検証コードは {####} です。',
+    EmailVerificationSubject=' 検証コード',
+    VerificationMessageTemplate={
+      'SmsMessage': ' 検証コードは {####} です。',
+      # Email利用はできなくなったみたい
+      # 'EmailMessage': ' 検証コードは {####} です。',
+      # 'EmailSubject': ' 検証コード',
+      'DefaultEmailOption': 'CONFIRM_WITH_CODE'
+    },
+    SmsAuthenticationMessage=' 認証コードは {####} です。',
+    MfaConfiguration='OFF',
+    # DeviceConfiguration={
+    #   'ChallengeRequiredOnNewDevice': True|False,
+    #   'DeviceOnlyRememberedOnUserPrompt': True|False
+    # },
+    # EmailConfiguration={
+    #   'SourceArn': 'string',
+    #   'ReplyToEmailAddress': 'string'
+    # },
+    # SmsConfiguration={
+    #   'SnsCallerArn': 'string',
+    #   'ExternalId': 'string'
+    # },
+    # UserPoolTags={
+    #   'string': 'string'
+    # },
+    AdminCreateUserConfig={
+      'AllowAdminCreateUserOnly': False,
+      'UnusedAccountValidityDays': 7,
+      'InviteMessageTemplate': {
+        'SMSMessage': ' ユーザー名は {username}、仮パスワードは {####} です。',
+        'EmailMessage': ' ユーザー名は {username}、仮パスワードは {####} です。',
+        'EmailSubject': ' 仮パスワード'
+      }
+    },
+    # Schema=[
+    #   {
+    #     'Name': 'string',
+    #     'AttributeDataType': 'String'|'Number'|'DateTime'|'Boolean',
+    #     'DeveloperOnlyAttribute': True|False,
+    #     'Mutable': True|False,
+    #     'Required': True|False,
+    #     'NumberAttributeConstraints': {
+    #       'MinValue': 'string',
+    #       'MaxValue': 'string'
+    #     },
+    #     'StringAttributeConstraints': {
+    #       'MinLength': 'string',
+    #       'MaxLength': 'string'
+    #     }
+    #   },
+    # ],
+    # UserPoolAddOns={
+    #   'AdvancedSecurityMode': 'OFF'|'AUDIT'|'ENFORCED'
+    # }
+  )
+  return response
+
+
+def _create_user_pool_app_client(client, user_pool):
+  user_pool_id = user_pool['UserPool']['Id']
+  user_pool_name = user_pool['UserPool']['Name']
+  response = client.create_user_pool_client(
+    UserPoolId=user_pool_id,
+    ClientName=f'{user_pool_name}-client',
+    # GenerateSecret=True|False,
+    RefreshTokenValidity=30,
+    # ReadAttributes=[
+    #   'string',
+    # ],
+    # WriteAttributes=[
+    #   'string',
+    # ],
+    # ExplicitAuthFlows=[
+    #   'ADMIN_NO_SRP_AUTH'|'CUSTOM_AUTH_FLOW_ONLY'|'USER_PASSWORD_AUTH',
+    # ],
+    # SupportedIdentityProviders=[
+    #   'string',
+    # ],
+    # CallbackURLs=[
+    # 'string',
+    # ],
+    # LogoutURLs=[
+    #   'string',
+    # ],
+    # DefaultRedirectURI='string',
+    # AllowedOAuthFlows=[
+    #   'code'|'implicit'|'client_credentials',
+    # ],
+    # AllowedOAuthScopes=[
+    #   'string',
+    # ],
+    # AllowedOAuthFlowsUserPoolClient=True|False,
+    # AnalyticsConfiguration={
+    #   'ApplicationId': 'string',
+    #   'RoleArn': 'string',
+    #   'ExternalId': 'string',
+    #   'UserDataShared': True|False
+    # }
+  )
+  return response
+
+
+def _set_user_pool_mfa_config(client, user_pool):
+  user_pool_id = user_pool['UserPool']['Id']
+  response = client.set_user_pool_mfa_config(
+    UserPoolId=user_pool_id,
+    # SmsMfaConfiguration={
+    #   'SmsAuthenticationMessage': 'string',
+    #   'SmsConfiguration': {
+    #     'SnsCallerArn': 'string',
+    #     'ExternalId': 'string'
+    #   }
+    # },
+    SoftwareTokenMfaConfiguration={
+      'Enabled': True # True|False
+    },
+    MfaConfiguration='ON' # 'OFF'|'ON'|'OPTIONAL'
+  )
+  return response
+
+
+def _create_user(client, user_pool, user_name, password):
+  user_pool_id = user_pool['UserPool']['Id']
+  response = client.admin_create_user(
+    UserPoolId=user_pool_id,
+    Username=user_name,
+    # UserAttributes=[
+    #   {
+    #     'Name': 'string',
+    #     'Value': 'string'
+    #   },
+    # ],
+    # ValidationData=[
+    #   {
+    #     'Name': 'string',
+    #     'Value': 'string'
+    #   },
+    # ],
+    TemporaryPassword=password,
+    # ForceAliasCreation=True|False,
+    MessageAction='SUPPRESS' # 'RESEND'|'SUPPRESS',
+    # DesiredDeliveryMediums=[
+    #   'SMS'|'EMAIL',
+    # ]
+  )
+  return response
+
+if __name__ == '__main__':
+  main()
